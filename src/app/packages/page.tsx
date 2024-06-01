376b8eb4227baef05e6082d7da9f1ae07acbe4c8
Joshua Hoffmann <joshuahoffmann60@gmail.com>
2024-06-01 04:28:12 +0200

    modified:   src/app/packages/page.tsx

--- src/app/packages/page.tsx
+++ src/app/packages/page.tsx
@@ -4,31 +4,34 @@ import Button from "../components/Button";
 import ContactForm from "../components/ContactForm";
 import PackCard from "../components/PackCard";
 import packData from "../array/packData";
-import { Metadata as NextMetadata } from "next";
 
-export const metadata: NextMetadata = {
-  title: "Packages | Creative Cate",
-  description:
-    "Explore a range of tailored solutions designed to elevate your brand and online presence. ",
-};
+import Head from "next/head";
+
 
 export default function Packages() {
   return (
     <div className="pt-[117px] -z-10">
+      <Head>
+        <title>Packages | Creative Cate</title>
+        <meta
+          name="description"
+          content="Explore a range of tailored solutions designed to elevate your brand and online presence."
+        />
+      </Head>
       <section className="block lg:flex justify-between lg:pt-0 ">
         <div className="flex flex-col justify-center mb-8 px-4 md:mx-[5rem] lg:mx-[4rem] lg:w-[50%] 2xl:mx-[20rem]">
           <h2 className="mb-6 md:my-10  lg:mb-8">Our Packages</h2>
           <p className="mb-14 text-secondary font-medium">
-            Explore a range of tailored solutions designed to
-            elevate your brand and online presence. From comprehensive branding
-            packages that define your identity to expertly crafted social media
-            strategies that engage your audience, each offering is meticulously
-            curated to meet your specific needs. Dive into the world of digital
-            marketing with ease through our website creation packages, designed
-            to establish a strong online presence and drive meaningful
-            connections with your target audience. With these packages, you're
-            not just investing in services; you're investing in the future
-            success of your business.
+            Explore a range of tailored solutions designed to elevate your brand
+            and online presence. From comprehensive branding packages that
+            define your identity to expertly crafted social media strategies
+            that engage your audience, each offering is meticulously curated to
+            meet your specific needs. Dive into the world of digital marketing
+            with ease through our website creation packages, designed to
+            establish a strong online presence and drive meaningful connections
+            with your target audience. With these packages, you're not just
+            investing in services; you're investing in the future success of
+            your business.
           </p>
         </div>
         <div className="relative -z-10 w-[100%] h-[380px] lg:h-[630px] lg:w-[50%] ">
